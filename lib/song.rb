90a26fe9911c8cb8ad7278a72392683cb73c1e54
Justin Davis <davisjustinw@gmail.com>
2019-01-11 00:06:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
   def self.genre_count
     genre_hash = {}
     @@genres.each do |genre|
-      genre_hash[genre] ||= 
+      genre_hash[genre] ||= genre_hash[genre] + 1
     end
   end
   
